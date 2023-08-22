d988233f84db422f4cc7aadca5f7dcec5179bfdb
Leonardo Gomes <leonardosg09@gmail.com>
2023-08-21 22:04:09 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+import Home from './screens/Home';
+import StarMap from './screens/StarMap';
+import DailyPic from './screens/DailyPic';
+import SpaceCraft from './screens/SpaceCraft';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="StarMap" component={StarMap}/>
+        <Stack.Screen name="DailyPic" component={DailyPic}/>
+        <Stack.Screen name="SpaceCraft" component={SpaceCraft}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ screens/DailyPic.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class DailyPic extends Component {
+  render() {
+    return (
+      <View
+        style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+        <Text>Tela das imagens diárias!</Text>
+      </View>
+    );
+  }
+}
